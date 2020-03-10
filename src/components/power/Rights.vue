8276d4b363c519e550176048c17b087efc22b35f
Gudongdong-SKY-20190201IYX <947044567@qq.com>
2020-03-10 14:18:53 +0800

    rights完成

---
+++ src/components/power/Rights.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="rights">
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/welcome' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-table :data="rightsList" border stripe>
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column label="权限名称" prop="authName"></el-table-column>
+        <el-table-column label="路径" prop="path"></el-table-column>
+        <el-table-column label="权限等级" prop="level">
+          <template v-slot:default="{row}">
+            <el-tag v-if="row.level==='0'">一级</el-tag>
+            <el-tag v-else-if="row.level==='1'" type="success">二级</el-tag>
+            <el-tag v-else type="warning">三级</el-tag>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      rightsList: []
+    }
+  },
+  created() {
+    this.getRightsList()
+  },
+  methods: {
+    async getRightsList() {
+      const { data: res } = await this.$axios.get('rights/list')
+      if (res.meta.status === 200) {
+        this.rightsList = res.data
+        console.log(this.rightsList)
+      } else {
+        this.$message.error(res.meta.msg)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, MessageBox, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Pagination, Dialog } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, MessageBox, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Pagination, Dialog, Tag } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -23,5 +23,6 @@ Vue.use(TableColumn)
 Vue.use(Switch)
 Vue.use(Pagination)
 Vue.use(Dialog)
+Vue.use(Tag)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
